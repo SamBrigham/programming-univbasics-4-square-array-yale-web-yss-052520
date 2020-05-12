ff68ebe781ae59ba6c13a90c073ecdab7ee09462
Samuel Brigha <sam.brigham@yale.edu>
2020-05-12 19:50:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
  count = 0
  new_array = []
- while count <= array.length do
+ while count < array.length do
    new_array.push((array[count])**2)
    count += 1
  end
- new_array
+return new_array
 end
